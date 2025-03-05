f9b99a4637ac0883773b7846d3013d0bbb7bc30e
VadymK <vadym.korolov@nekken.pl>
2025-03-05 21:38:24 +0100

    ver 0.2.1 - aktualizacja danych

--- src/main/resources/db/sql/2025_03_05_21_09_drop_audit_columns.sql
+++ src/main/resources/db/sql/2025_03_05_21_09_drop_audit_columns.sql
@@ -5,29 +5,40 @@ alter table adresy
     drop column if exists create_date,
     drop column if exists created_by,
     drop column if exists last_modified_date,
-    drop column if exists last_modified_by;
+    drop column if exists last_modified_by,
+    drop column if exists version_id;
 
 
 alter table kraje
     drop column if exists create_date,
     drop column if exists created_by,
     drop column if exists last_modified_date,
-    drop column if exists last_modified_by;
+    drop column if exists last_modified_by,
+    drop column if exists version_id;
 
 alter table osoby
     drop column if exists create_date,
     drop column if exists created_by,
     drop column if exists last_modified_date,
-    drop column if exists last_modified_by;
+    drop column if exists last_modified_by,
+    drop column if exists version_id;
 
 alter table kody_pkd
     drop column if exists create_date,
     drop column if exists created_by,
     drop column if exists last_modified_date,
-    drop column if exists last_modified_by;
+    drop column if exists last_modified_by,
+    drop column if exists version_id;
 
 alter table lista_jdg_pobieranie
     drop column if exists created_by,
     drop column if exists last_modified_date,
-    drop column if exists last_modified_by;
+    drop column if exists last_modified_by,
+    drop column if exists version_id;
+
+alter table jednoosobowe_dzialalnosci_gospodarcze
+    drop column if exists created_by,
+    drop column if exists last_modified_date,
+    drop column if exists last_modified_by,
+    drop column if exists version_id;
 
