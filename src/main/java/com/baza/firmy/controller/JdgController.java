e2bcd5667d36c1e31ce01338b9c1e9c0c8463683
VadymK <vadym.korolov@nekken.pl>
2025-03-07 21:24:09 +0100

    ver 0.2.1 - fixed swagger

--- src/main/java/com/baza/firmy/controller/JdgController.java
+++ src/main/java/com/baza/firmy/controller/JdgController.java
@@ -1,17 +1,18 @@
 package com.baza.firmy.controller;
 
-import com.baza.firmy.repository.JdgFilterSpecification;
 import com.baza.firmy.dto.JdgListDto;
+import com.baza.firmy.repository.JdgFilterSpecification;
 import com.baza.firmy.service.JdgService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
--- src/main/java/com/baza/firmy/repository/JdgRepository.java
+++ src/main/java/com/baza/firmy/repository/JdgRepository.java
@@ -1,16 +1,18 @@
 package com.baza.firmy.repository;
 
 import com.baza.firmy.entity.Jdg;
-import java.util.Optional;
-import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface JdgRepository extends JpaRepository<Jdg, Long>, JpaSpecificationExecutor<Jdg> {
 
   boolean existsByCeidgId(UUID ceidgId);
+
   Optional<Jdg> findByCeidgId(UUID ceidgId);
 
 }
