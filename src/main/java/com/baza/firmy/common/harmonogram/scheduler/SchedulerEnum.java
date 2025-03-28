8ca0144212073cf527011dd4602def71a86ed8af
VadymK <vadym.korolov@nekken.pl>
2025-03-28 11:43:40 +0100

    ver 0.3.3 - aktualizacja pobierania szczegółów

--- src/main/java/com/baza/firmy/common/harmonogram/scheduler/SchedulerEnum.java
+++ src/main/java/com/baza/firmy/common/harmonogram/scheduler/SchedulerEnum.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.quartz.QuartzJobBean;
 @Getter
 enum SchedulerEnum {
   TEST_SCHEDULER("TEST_TRIGGER", "TEST_JOB", "0 0/1 * * * ?", TestJob.class),
-  POBIERAJ_LISTE_JDG_OST_DOBA_SCHEDULER("POBIERAJ_LISTE_JDG_OST_DOBA_TRIGGER", "POBIERAJ_LISTE_JDG_OST_DOBA_JOB", "0 37 11 * * ?", PobierajListeJDGOstDobaJob.class);
+  POBIERAJ_LISTE_JDG_OST_DOBA_SCHEDULER("POBIERAJ_LISTE_JDG_OST_DOBA_TRIGGER", "POBIERAJ_LISTE_JDG_OST_DOBA_JOB", "0 50 11 * * ?", PobierajListeJDGOstDobaJob.class);
 
   private String triggerKod;
   private String jobKod;
