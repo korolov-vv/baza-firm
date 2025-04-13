39d50a03e2f843175a66d5849000b6f27b6bddd6
VadymK <vadym.korolov@nekken.pl>
2025-04-13 22:41:09 +0200

    ver 0.1.1 - fix

--- src/main/java/com/baza/firmy/common/harmonogram/scheduler/WyslijListeJdgNoweService.java
+++ src/main/java/com/baza/firmy/common/harmonogram/scheduler/WyslijListeJdgNoweService.java
@@ -17,7 +17,7 @@ class WyslijListeJdgNoweService implements BazowySchedulerService {
  @Override
  public void executeScheduler(JobExecutionContext jobExecutionContext) {
   mailSenderService.sendMessageWithFirms(
-      "vadimcoffee@gmail.com",
+      "k.grabowski@schrack.pl",
       "Lista JDG",
       String.format("Cześć! W załączniku firmy z %s", LocalDate.now().minusDays(3)));
  }
