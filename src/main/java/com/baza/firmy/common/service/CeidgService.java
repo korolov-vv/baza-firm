6eb0a74012b10240e14a6517dc94f67090d4dfbb
Vadym Korolov <60427025+korolov-vv@users.noreply.github.com>
2025-09-15 22:04:51 +0200

    3.3.0 - aktualizacja integracji z CEIDG

--- src/main/java/com/baza/firmy/common/service/CeidgService.java
+++ src/main/java/com/baza/firmy/common/service/CeidgService.java
@@ -1,5 +1,6 @@
 package com.baza.firmy.common.service;
 
+import com.baza.firmy.configuration.properties.CeidgProperties;
 import com.baza.firmy.integration.ceidg.CeidgClient;
 import com.baza.firmy.response.Dto;
 import com.baza.firmy.response.ListaJdgDto;
@@ -14,13 +15,17 @@ class CeidgService {
   
   private final CeidgClient ceidgClient;
 
+  private final CeidgProperties ceidgProperties;
+
   public ListaJdgDto pobierzListeJdg(String link) {
     return ceidgClient.pobierzListeJdg(link);
   }
 
   public Dto pobierzSzczegolyJdg(String link) {
     final var response = ceidgClient.pobierzSzczegolyJdg(link);
-    log.info("Pobrano szczegóły JDG link: {},\n response: {}", link, response);
+    log.info("Pobrano szczegóły JDG link: {},\n response: {}",
+            link.replace("https://dane.biznes.gov.pl/api/ceidg/v2", ceidgProperties.getCeidgPath()),
+            response);
     return response;
   }
 }
