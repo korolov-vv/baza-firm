c789b15f74f4066b46ddea1f56e84b66d729cc51
Vadym Korolov <vadymkorolov@gmail.com>
2026-02-23 22:53:09 +0100

    Poprawka metody main

--- src/main/java/com/baza/firmy/FirmsApplication.java
+++ src/main/java/com/baza/firmy/FirmsApplication.java
@@ -10,8 +10,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 @EnableSpringDataWebSupport (pageSerializationMode = VIA_DTO)
 public class FirmsApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(FirmsApplication.class, args);
 	}
-
 }
