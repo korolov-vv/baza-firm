93a40915b5a2069047c6ae915e7a9d9087128671
Vadym Korolov <vadymkorolov@gmail.com>
2026-03-25 21:13:11 +0100

    podbicie wersji

--- src/main/java/com/baza/firmy/common/harmonogram/scheduler/TestSchedulerService.java
+++ src/main/java/com/baza/firmy/common/harmonogram/scheduler/TestSchedulerService.java
@@ -17,9 +17,9 @@ public class TestSchedulerService implements BazowySchedulerService {
 
   public void executeScheduler(JobExecutionContext jobExecutionContext) {
     try {
-      log.info("Start test job 0.4.0");
+      log.info("Start test job 1.0.0");
       TimeUnit.SECONDS.sleep(10);
-      log.info("Koniec test job 0.4.0");
+      log.info("Koniec test job 1.0.0");
     } catch (InterruptedException exception) {
       log.error("Test job error", exception);
     }
